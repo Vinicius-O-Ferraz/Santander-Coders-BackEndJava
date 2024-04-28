7f3a3917803c8b980cd93bbac41fed78636800cf
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2024-04-28 13:24:38 -0300

    end of workday

--- code/calculator/src/main.java
+++
@@ -1,7 +0,0 @@
-public static void main(String[] args) {
-    Calculator calculator = new Calculator();
-    int result = calculator.sum(1,3);
-    System.out.println(result);
-    result = calculator.sum(3,1);
-    System.out.println(result);
-}
---
+++ code/java-terminal/src/MinhaClasse.java
@@ -0,0 +1,7 @@
+public class MinhaClasse {
+    
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Hello, I'm excited to learn Java!");
+    }
+}
