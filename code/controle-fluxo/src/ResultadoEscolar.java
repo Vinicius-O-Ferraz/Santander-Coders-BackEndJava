fd056caa52612ead4e7800cdbbfc386bd6e6fe2a
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2024-04-29 20:22:51 -0300

    implemented java class

--- code/controle-fluxo/src/ResultadoEscolar.java
+++ code/controle-fluxo/src/ResultadoEscolar.java
@@ -1,8 +1,7 @@
 public class ResultadoEscolar {
 	public static void main(String[] args) {
-		int nota = 5;
-		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
-		System.out.println();
-        System.out.println(resultado);
+		int nota = 6;
+		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado);
 	}
 }
