224916cdac12fce620bb932f03766b2c7555aaa8
Vinícius Ferreira Ferraz <146992032+Vinicius-O-Ferraz@users.noreply.github.com>
2024-04-30 17:18:47 -0300

    Update App.java

--- code/controle-candidatos/src/App.java
+++ code/controle-candidatos/src/App.java
@@ -5,14 +5,21 @@ public class App {
 
         System.out.println();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Informe o valor do salário base:  ");
+        System.out.println("Informe o valor do salario base:  ");
         int salarioBase = scanner.nextInt();
-        System.out.println("Informe o valor do salário pretendido pelo candidato:  ");
+        System.out.println("Informe o valor do salario pretendido pelo candidato:  ");
         int salarioPretendidoPeloCandidato = scanner.nextInt();
 
-        System.err.println(salarioBase);
-        System.out.println(salarioPretendidoPeloCandidato);
-
         scanner.close();
+        
+    if (salarioBase>salarioPretendidoPeloCandidato){
+        System.out.println("Ligar para o candidato");
+    }
+    else if (salarioBase==salarioPretendidoPeloCandidato){
+        System.out.println("Ligar para o candidato, com contra proposta");
+    }
+    else{
+        System.out.println("Aguardando resultado dos demais candidatos");
+    }
     }
 }
